7efaaf7b24488edb1d4f6c45a6c9fed267983f9a
irinka datoshvili <irina.datoshvili.1@btu.edu.ge>
2024-01-23 20:16:20 +0400

    Initial Commit

---
+++ P1/P1App.swift
@@ -0,0 +1,17 @@
+//
+//  P1App.swift
+//  P1
+//
+//  Created by Irinka Datoshvili on 23.01.24.
+//
+
+import SwiftUI
+
+@main
+struct P1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
