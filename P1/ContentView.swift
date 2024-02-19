b87b0c6cdcca93e6c2e011c1c004558820989a40
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-02-19 13:22:26 +0400

    Togglable isFaceUp

--- P1/ContentView.swift
+++ P1/ContentView.swift
@@ -23,19 +23,22 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = false
+    
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 14)
-                    .strokeBorder(style: StrokeStyle(lineWidth: 2))
-                    .foregroundColor(.orange)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
             }else{
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
+        }
+        .onTapGesture {
+//            isFaceUp = !isFaceUp
+            isFaceUp.toggle()
         }
     }
 }
