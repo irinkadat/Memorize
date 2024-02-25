a048885b9aca4cf00b3f1d7e36363129be71ceb9
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-02-25 23:21:07 +0400

    Add Animation To ContentView

--- P1/MemoryGame.swift
+++ P1/MemoryGame.swift
@@ -9,7 +9,7 @@
 
 import Foundation
 
-struct MemoryGame<CardContent>{
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
     
     init(numberOfPairsOfCards:Int, cardContentFactory: (Int) -> CardContent) {
@@ -17,8 +17,8 @@ struct MemoryGame<CardContent>{
         
         for pairIndex in 0..<max(2,numberOfPairsOfCards) {
             let content : CardContent = cardContentFactory(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: "\(pairIndex+1)a"))
+            cards.append(Card(content: content,id: "\(pairIndex+1)b"))
 
         }
     }
@@ -32,9 +32,19 @@ struct MemoryGame<CardContent>{
         
     }
 
-    struct Card{
+    struct Card:Equatable, Identifiable{
+        
+//        static func == (lhs: Card, rhs: Card) -> Bool {
+//            return lhs.isFaceUp == rhs.isFaceUp &&
+//            lhs.isMatched == rhs.isMatched &&
+//            lhs.content == rhs.content
+//        }
+        
         var isFaceUp = true
         var isMatched = false
         var content : CardContent
+        
+        var id: String
+
     }
 }
